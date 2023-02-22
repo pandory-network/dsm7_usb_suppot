4654aa7276681aa7d90d6516893f69e45aebca72
Pandory <pandory-network@users.noreply.github.com>
2023-02-22 21:26:33 +0100

    Update dsm7_enable_usb_support.sh

--- dsm7_usb_suppot/dsm7_enable_usb_support.sh
+++ dsm7_usb_suppot/dsm7_enable_usb_support.sh
@@ -11,19 +11,19 @@ modprobe cdc-acm
 
 cd /lib/modules/
 
-sudo wget https://web.archive.org/web/20211027134846/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/apollolake/ch341.ko
+sudo wget https://web.archive.org/web/20211027141018/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/geminilake/ch341.ko
 sudo insmod /lib/modules/ch341.ko
-sudo wget https://web.archive.org/web/20211027134846/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/apollolake/cp210x.ko
+sudo wget https://web.archive.org/web/20211027141018/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/geminilake/cp210x.ko
 sudo insmod /lib/modules/cp210x.ko
-sudo wget https://web.archive.org/web/20211027134846/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/apollolake/pl2303.ko
+sudo wget https://web.archive.org/web/20211027141018/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/geminilake/pl2303.ko
 sudo insmod /lib/modules/pl2303.ko
-sudo wget https://web.archive.org/web/20211027134846/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/apollolake/ti_usb_3410_5052.ko
+sudo wget https://web.archive.org/web/20211027141018/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/geminilake/ti_usb_3410_5052.ko
 sudo insmod /lib/modules/ti_usb_3410_5052.ko
 
 # Configure an autoload script
 
 cd  /usr/local/etc/rc.d/
-sudo wget https://web.archive.org/web/20211027134846/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/apollolake/start-usb-drivers.sh
+sudo wget https://web.archive.org/web/20211027141018/http://www.jadahl.com/iperf-arp-scan/DSM_7.0/geminilake/start-usb-drivers.sh
 sudo chmod +x start-usb-drivers.sh
 
 # Make sure your devices are recongnized
